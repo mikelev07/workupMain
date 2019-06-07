da340336c44117152a44f794392140a766c623b9
mikelev07 <45894165+mikelev07@users.noreply.github.com>
2019-06-07 13:24:59 +0300

    else fix

--- HelpMe/HelpMe/App_Start/IdentityConfig.cs
+++ HelpMe/HelpMe/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using HelpMe.Models;
+using System.Net.Mail;
 
 namespace HelpMe
 {
@@ -18,8 +19,25 @@ namespace HelpMe
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Подключите здесь службу электронной почты для отправки сообщения электронной почты.
-            return Task.FromResult(0);
+            // настройка логина, пароля отправителя
+            var from = "classicfrontend2018@gmail.com";
+            var pass = "front1234";
+
+            // адрес и порт smtp-сервера, с которого мы и будем отправлять письмо
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 465);
+
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(from, pass);
+            client.EnableSsl = true;
+
+            // создаем письмо: message.Destination - адрес получателя
+            var mail = new MailMessage(from, message.Destination);
+            mail.Subject = message.Subject;
+            mail.Body = message.Body;
+            mail.IsBodyHtml = true;
+
+            return client.SendMailAsync(mail);
         }
     }
 
