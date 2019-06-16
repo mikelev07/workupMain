645962007ec43802c188c75a32ad23c7470fc765
EmilioGitHub <davletovemil@yandex.ru>
2019-06-16 16:32:05 +0700

    StarRatingFunction
    change for 0.5 and 0 rating

--- HelpMe/HelpMe/Scripts/Custom/custom.js
+++ HelpMe/HelpMe/Scripts/Custom/custom.js
@@ -432,13 +432,14 @@ $(document).ready(function(){
 			var dataRating = $(this).attr('data-rating');
 
 			// Rating Stars Output
-			function starsOutput(firstStar, secondStar, thirdStar, fourthStar, fifthStar) {
-				return(''+
-					'<span class="'+firstStar+'"></span>'+
-					'<span class="'+secondStar+'"></span>'+
-					'<span class="'+thirdStar+'"></span>'+
-					'<span class="'+fourthStar+'"></span>'+
-					'<span class="'+fifthStar+'"></span>');
+			function starsOutput(zeroStar,firstStar, secondStar, thirdStar, fourthStar, fifthStar) {
+                return ('' +
+                    '<span class="' + zeroStar + '"></span>' +
+					'<span class="' + firstStar+ '"></span>' +
+					'<span class="'+ secondStar+'"></span>'+
+					'<span class="'+ thirdStar +'"></span>'+
+					'<span class="'+ fourthStar+'"></span>'+
+					'<span class="'+ fifthStar +'"></span>');
 			}
 
 			var fiveStars = starsOutput('star','star','star','star','star');
@@ -453,28 +454,33 @@ $(document).ready(function(){
 			var twoStars = starsOutput('star','star','star empty','star empty','star empty');
 
 			var oneHalfStar = starsOutput('star','star half','star empty','star empty','star empty');
-			var oneStar = starsOutput('star','star empty','star empty','star empty','star empty');
+            var oneStar = starsOutput('star', 'star empty', 'star empty', 'star empty', 'star empty');
+
+            var halfStar = starsOutput('star half', 'star empty', 'star empty', 'star empty', 'star empty');
+            var zeroStar = starsOutput('star empty', 'star empty', 'star empty', 'star empty', 'star empty');
 
 			// Rules
-	        if (dataRating >= 4.75) {
-	            $(this).append(fiveStars);
-	        } else if (dataRating >= 4.25) {
-	            $(this).append(fourHalfStars);
-	        } else if (dataRating >= 3.75) {
-	            $(this).append(fourStars);
-	        } else if (dataRating >= 3.25) {
-	            $(this).append(threeHalfStars);
-	        } else if (dataRating >= 2.75) {
-	            $(this).append(threeStars);
-	        } else if (dataRating >= 2.25) {
-	            $(this).append(twoHalfStars);
-	        } else if (dataRating >= 1.75) {
-	            $(this).append(twoStars);
-	        } else if (dataRating >= 1.25) {
-	            $(this).append(oneHalfStar);
-	        } else if (dataRating < 1.25) {
-	            $(this).append(oneStar);
-	        }
+            if (dataRating >= 4.75) {
+                $(this).append(fiveStars);
+            } else if (dataRating >= 4.25) {
+                $(this).append(fourHalfStars);
+            } else if (dataRating >= 3.75) {
+                $(this).append(fourStars);
+            } else if (dataRating >= 3.25) {
+                $(this).append(threeHalfStars);
+            } else if (dataRating >= 2.75) {
+                $(this).append(threeStars);
+            } else if (dataRating >= 2.25) {
+                $(this).append(twoHalfStars);
+            } else if (dataRating >= 1.75) {
+                $(this).append(twoStars);
+            } else if (dataRating >= 1.25) {
+                $(this).append(oneHalfStar);
+            } else if (dataRating >= 0.75) {
+                $(this).append(oneStar);
+            } else if (dataRating >= 0.25) {
+                $(this).append(halfStar);
+            } else $(this).append(zeroStar);
 
 		});
 
