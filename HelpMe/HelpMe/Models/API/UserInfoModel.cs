3373684632ef34f598010f4ef2c22911ea6ff859
mikelev07 <45894165+mikelev07@users.noreply.github.com>
2019-09-07 13:25:09 +0300

    add accountApi

---
+++ HelpMe/HelpMe/Models/API/UserInfoModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelpMe.Models.API
+{
+
+    public class AddExternalLoginBindingModel
+    {
+        [Required]
+        [Display(Name = "Внешний маркер доступа")]
+        public string ExternalAccessToken { get; set; }
+    }
+
+    public class ChangePasswordBindingModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Текущий пароль")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение нового пароля")]
+        [Compare("NewPassword", ErrorMessage = "Новый пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class RegisterBindingModel
+    {
+        [Required]
+        [Display(Name = "Адрес электронной почты")]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class RegisterExternalBindingModel
+    {
+        [Required]
+        [Display(Name = "Адрес электронной почты")]
+        public string Email { get; set; }
+    }
+
+    public class RemoveLoginBindingModel
+    {
+        [Required]
+        [Display(Name = "Поставщик входа")]
+        public string LoginProvider { get; set; }
+
+        [Required]
+        [Display(Name = "Ключ поставщика")]
+        public string ProviderKey { get; set; }
+    }
+
+    public class SetPasswordBindingModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение нового пароля")]
+        [Compare("NewPassword", ErrorMessage = "Новый пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class ExternalLoginViewModel
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string State { get; set; }
+    }
+    public class UserLoginInfoViewModel
+    {
+        public string LoginProvider { get; set; }
+
+        public string ProviderKey { get; set; }
+    }
+    public class ManageInfoViewModel
+    {
+        public string LocalLoginProvider { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<UserLoginInfoViewModel> Logins { get; set; }
+
+        public IEnumerable<ExternalLoginViewModel> ExternalLoginProviders { get; set; }
+    }
+    public class UserInfoViewModel
+    {
+        public string Email { get; set; }
+
+        public bool HasRegistered { get; set; }
+
+        public string LoginProvider { get; set; }
+    }
+}
